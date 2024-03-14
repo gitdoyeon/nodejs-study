0f0981b58ec683ac86e4122d266a2b9541145ffb
gitdoyeon <gitpdyeon@gmail.com>
2024-03-14 15:01:18 +0900

    commit

---
+++ ex02.js
@@ -0,0 +1,18 @@
+// let
+function letTest() {
+    let ll = 1;
+    if(true) {
+        let ll = 2;
+        console.log(ll);
+    }
+}
+le.log("var 실행");
+
+// var
+function varTest() {
+    var vv = 1;
+    if(true) {
+        var vv = 2;
+    }
+    console.log(vv);
+}
---
+++ ex03.js
@@ -0,0 +1,10 @@
+
+
+console.log("Data Types");
+console.log("null : "+Boolean(null));
+console.log("NaN : "+Boolean(NaN));
+console.log("0 : "+Boolean(0));
+console.log("반문자열 : "+Boolean(""));
+console.log("반문자열 : "+Boolean(''));
+console.log("반문자열 : "+Boolean(``));
+console.log("undefined : "+Boolean(undefined));
